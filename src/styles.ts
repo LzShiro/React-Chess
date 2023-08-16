e400ead3033611f30af85fa6649bb49e210675fa
Argos Piña <argospa97@gmail.com>
2023-08-15 21:21:58 -0600

    styles created

---
+++ src/styles.ts
@@ -0,0 +1,12 @@
+import {Dimensions} from 'react-native';
+
+export const windowWidth = Dimensions.get('window').width;
+export const windowHeight = Dimensions.get('window').height;
+
+export const margins = {
+  xSmall: 4,
+  small: 8,
+  medium: 16,
+  large: 24,
+  xLarge: 32,
+};
